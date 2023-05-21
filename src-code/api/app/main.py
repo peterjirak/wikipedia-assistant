3c0efa649b63c0d8dad7f4f164a0ceca66cfe0f6
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 09:25:20 -0700

    I am using the Simple Serverless FastAPI with AWS Lambda tutorial by Sean Baier to set up my API routes. So this is my initial configuration as taken from that tutorial.

--- src-code/api/app/main.py
+++ src-code/api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.api.api_v1.api import router as api_router
 
 app = FastAPI()
 
@@ -7,6 +8,4 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/users")
-async def get_users():
-    return {"message": "Get Users!"}
+app.include_router(api_router, prefix="/api/v1")
