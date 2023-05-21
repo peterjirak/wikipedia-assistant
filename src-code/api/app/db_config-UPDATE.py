2093624023978489c774456e92f6e178f4e01ea9
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 15:05:29 -0700

    Renamed src-code/api/app/db_config-UPDATE.py -> src-code/api/app/db_connection_url-UPDATE.py. Added the function get_db_connection_url

--- src-code/api/app/db_config-UPDATE.py
+++
@@ -1,8 +0,0 @@
-def get_db_config():
-    return {
-        'host': 'HOST',
-        'user': 'USER',
-        'password': 'PASSWORD',
-        'port': 0000,
-        'database': 'DATABASE'
-    }
---
+++ src-code/api/app/db_connection_url-UPDATE.py
@@ -0,0 +1,16 @@
+
+# f"mysql+pymysql://{connection_info['user']}:{connection_info['password']}@{connection_info['host']}:{connection_info['port']}/{connection_info['database']}?charset=utf8mb4"
+
+def get_db_config():
+    return {
+        'host': 'HOST',
+        'user': 'USER',
+        'password': 'PASSWORD',
+        'port': 9999,
+        'database': 'lynx_analytics_wikipedia_assistant'
+    }
+
+def get_db_connection_url():
+    db_config = get_db_config()
+    db_url = f"mysql+asyncmy://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}?charset=utf8mb4"
+    return db_url
