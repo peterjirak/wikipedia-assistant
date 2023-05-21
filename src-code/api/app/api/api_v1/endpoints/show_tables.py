2f40b37d95315c482d4e4284bfbf0e51dcf2857a
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 11:19:47 -0700

    Fixed an issue in the connection string. Was missing the port number. Added in the port number.

--- src-code/api/app/api/api_v1/endpoints/show_tables.py
+++ src-code/api/app/api/api_v1/endpoints/show_tables.py
@@ -9,7 +9,7 @@ router = APIRouter()
 async def root():
     tables = []
     connection_info = get_db_config()
-    engine = sqlalchemy.create_engine(f"mysql+pymysql://{connection_info['user']}:{connection_info['password']}@{connection_info['host']}/{connection_info['database']}?charset=utf8mb4")
+    engine = sqlalchemy.create_engine(f"mysql+pymysql://{connection_info['user']}:{connection_info['password']}@{connection_info['host']}:{connection_info['port']}/{connection_info['database']}?charset=utf8mb4")
 
     with engine.connect() as connection:
         query_result = connection.execute(text('show tables'));
