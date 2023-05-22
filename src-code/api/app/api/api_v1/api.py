456c8f545cd8545434f56257cdf47308f16eee21
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 19:23:02 -0700

    Changes:  1. Removed the API /api/v1/users. It was never relevant to this programming assignment. I had added it early in the implementation of my API as a part of understanding, testing, debugging the implementation of my API using Python and FastAPI.  2. Added support for the API /api/v1/table-row-count/{table_name} . This API takes in a table name and returns the number of rows in that table.

--- src-code/api/app/api/api_v1/api.py
+++ src-code/api/app/api/api_v1/api.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from .endpoints import users, current_utc_datetime, show_tables, hello, hello_world, query
+from .endpoints import current_utc_datetime, show_tables, hello, hello_world, query, table_row_count
 
 router = APIRouter()
-router.include_router(users.router, prefix='/users', tags=['Users'])
 router.include_router(current_utc_datetime.router, prefix='/current-utc-datetime', tags=['CurrentUtcDatetime'])
 router.include_router(show_tables.router, prefix='/show-tables', tags=['ShowTables'])
 router.include_router(hello.router, prefix='/hello', tags=['Hello'])
 router.include_router(hello_world.router, prefix='/hello-world', tags=['HelloWorld'])
 router.include_router(query.router, prefix='/query', tags=['Query'])
+router.include_router(table_row_count.router, prefix='/table-row-count', tags=['TableRowCount'])
--- src-code/api/app/main.py
+++ src-code/api/app/main.py
@@ -1,42 +1,7 @@
-import re
-
-import sqlalchemy
-from sqlalchemy import text
-from sqlalchemy.ext.asyncio import create_async_engine
 from fastapi import FastAPI
 from app.db_connection_url import get_db_connection_url
 from app.api.api_v1.api import router as api_router
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/api/v1/table/{table_name}/row_count")
-async def get_row_count_for_table(table_name):
-    if not table_name:
-        raise ValueError("Bad call to API endpoint. A valid table name was not specified.")
-    table_name = f'{table_name}'
-    if not re.match(r"[A-Za-z0-9]", table_name):
-        raise ValueError("Bad call to API endpoint. A valid table name was not specified.")
-    table_name = table_name.strip()
-    if re.match(r"[^A-Za-z0-9_-]", table_name):
-        raise ValueError("Bad call to API end point. Table name parameter contains one or more invalid characters.")
-    if len(table_name) > 32:
-        raise ValueError("Bad call to API end point. A valid table name cannot exceed 32 characters.")
-    row_count = 0
-    db_url = get_db_connection_url()
-    engine = create_async_engine(db_url)
-    connection = await engine.connect()
-    query_result = await connection.execute(text(f'select count(*) from {table_name}'))
-    for row in query_result:
-        print(f'type(row): {type(row)}')
-        for value in row:
-            row_count = value
-
-    
-    return {"message": row_count}
 
 app.include_router(api_router, prefix="/api/v1")
