5a90697d56a238c619b1e7ca5d89810a77bcd936
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 17:23:48 -0700

    Modified the hello endpoint. It is now /api/v1/hello/{name}

--- src-code/api/app/api/api_v1/endpoints/hello.py
+++ src-code/api/app/api/api_v1/endpoints/hello.py
@@ -1,8 +1,15 @@
+import re
 from datetime import datetime, timezone
 from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/")
-async def root():
-    return {"message": 'Hello, World!'}
+@router.get("/{name}")
+async def root(name):
+    greeting = ''
+    if not name or not re.match(r"\S", name):
+        greeting = 'Hello, World!'
+    else:
+        name = name.strip()
+        greeting = f"Hello, {name}"
+    return {"message": greeting}
