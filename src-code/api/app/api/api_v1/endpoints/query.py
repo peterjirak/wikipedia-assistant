52613677f24a04e18f58b48504de3d4e68863e44
Peter Eldritch <peter.jirak@gmail.com>
2023-05-21 18:59:42 -0700

    Implemented src-code/api/app/api/api_v1/endpoints/query.py which will once added act as a handler for the API call /api/v1/query/{query}

---
+++ src-code/api/app/api/api_v1/endpoints/query.py
@@ -0,0 +1,25 @@
+import re
+import sqlalchemy
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine
+from fastapi import APIRouter
+from app.db_connection_url import get_db_connection_url
+
+router = APIRouter()
+
+@router.get("/{query}")
+async def root(query):
+    if (not query or
+        not re.match(r"\S", query) or
+        (not re.match(r"\bSELECT\b", query, flags=re.IGNORECASE) and not re.match(r"\bSHOW\b", query, re.IGNORECASE))):
+        raise ValueError("Bad call to API endpoint. A valid query was not specified.")
+    print(f'query: {query}')
+    matches = []
+    db_url = get_db_connection_url()
+    engine = create_async_engine(db_url)
+    connection = await engine.connect()
+    query_result = await connection.execute(text(query))
+    for row in query_result:
+        matches.append(row._mapping)
+    
+    return {"message": matches}
