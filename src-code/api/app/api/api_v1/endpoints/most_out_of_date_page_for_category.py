c0182b3ee4c83b0e387e331598c6ca94e42b33be
Peter Eldritch <peter.jirak@gmail.com>
2023-05-22 21:55:32 -0700

    Having difficulty with binding params so at this point I am using string interpolation which is a poor practice.

--- src-code/api/app/api/api_v1/endpoints/most_out_of_date_page_for_category.py
+++ src-code/api/app/api/api_v1/endpoints/most_out_of_date_page_for_category.py
@@ -11,17 +11,13 @@ router = APIRouter()
 async def root(category):
     if not category or not re.match(r"\S", category):
         raise ValueError("Bad call to API endpoint. A valid category was not specified.")
-    query = text("""
-                     SELECT *
-                     FROM most_outdated_page_for_ten_categories_with_most_pages
-                     WHERE cat_title = :category
-""")
+    query = text(f"SELECT * FROM most_outdated_page_for_ten_categories_with_most_pages WHERE cat_title = '{category}'")
     category = category.split()
     matches = []
     db_url = get_db_connection_url()
     engine = create_async_engine(db_url)
     connection = await engine.connect()
-    query_result = await connection.execute(query, category=category)
+    query_result = await connection.execute(query)
     for row in query_result:
         matches.append(row._mapping)
     
