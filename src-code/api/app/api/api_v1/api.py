e6e1d3c4e59879117e7bfd23abc3ecf2f5dbc628
Peter Eldritch <peter.jirak@gmail.com>
2023-05-22 21:37:54 -0700

    Working on part 2B pf the programming assignment. In an earlier revision I created the SQL queries and tables necessary for part 2B. I am now implementing the API necessary to execute a simple query on the table that contains the precomputed result I did in the earlier commit.

--- src-code/api/app/api/api_v1/api.py
+++ src-code/api/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .endpoints import current_utc_datetime, show_tables, hello, hello_world, query, table_row_count
+from .endpoints import current_utc_datetime, show_tables, hello, hello_world, query, table_row_count, most_out_of_date_page_for_category
 
 router = APIRouter()
 router.include_router(current_utc_datetime.router, prefix='/current-utc-datetime', tags=['CurrentUtcDatetime'])
@@ -9,3 +9,4 @@ router.include_router(hello.router, prefix='/hello', tags=['Hello'])
 router.include_router(hello_world.router, prefix='/hello-world', tags=['HelloWorld'])
 router.include_router(query.router, prefix='/query', tags=['Query'])
 router.include_router(table_row_count.router, prefix='/table-row-count', tags=['TableRowCount'])
+router.include_router(most_out_of_date_page_for_category.router, prefix='/most-out-of-date-page-for-category', tags=['MostOutOfDatePageForCategory'])
---
+++ src-code/api/app/api/api_v1/endpoints/most_out_of_date_page_for_category.py
@@ -0,0 +1,28 @@
+import re
+import sqlalchemy
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine
+from fastapi import APIRouter
+from app.db_connection_url import get_db_connection_url
+
+router = APIRouter()
+
+@router.get("/{category}")
+async def root(category):
+    if not category or not re.match(r"\S", category):
+        raise ValueError("Bad call to API endpoint. A valid category was not specified.")
+    query = text("""
+                     SELECT *
+                     FROM most_outdated_page_for_ten_categories_with_most_pages
+                     WHERE cat_title = :category
+""")
+    category = category.split()
+    matches = []
+    db_url = get_db_connection_url()
+    engine = create_async_engine(db_url)
+    connection = await engine.connect()
+    query_result = await connection.execute(query, category=category)
+    for row in query_result:
+        matches.append(row._mapping)
+    
+    return {"message": matches}
